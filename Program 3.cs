6a2049c40fa72ee9761c0993c8fdd7f954d27cfc
XyliLoced <143069617+XyliLoced@users.noreply.github.com>
2023-09-27 16:44:48 +0300

    Add files via upload

---
+++ Program 3.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] randomArray = new double[a];
+
+void mas(int a)
+{
+    Random rand = new Random();                      //random
+    for (int i = 0; i < a; i++)
+    {
+        randomArray[i] = rand.NextDouble();
+        Console.Write($"[{randomArray[i]:F2}] ");
+    }
+
+}
+
+double raz(double[] randomArray)
+{
+    double min = randomArray[0];
+    double max = randomArray[0];
+    int i = 1;
+    while (i < randomArray.Length)
+    {
+        if (max < randomArray[i])          //Разница массивов
+            max = randomArray[i];
+        if (min > randomArray[i])
+            min = randomArray[i];
+        i = i + 1;
+    }
+    return max - min;
+}
+
+mas(a); // Итог
+Console.Write($"разница= {raz(randomArray):F2}");
+//Немного не понял задания c вещественными чиселами, но вроде бы основное выполнил
+//точнее не разобрался чем отличаються от обычных, как я понял лежать в определённом отрезке и дробные
